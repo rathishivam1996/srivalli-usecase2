6d3b3e5f1f9a142fa112d7ee9cf46ce4f3ef20b5
Shivam Rathi <shivamrathi2.sr@gmail.co>
2023-03-22 00:22:31 +0530

    Exception Handling

--- src/main/java/com/srivalli/travelagency/dto/CustomerDto.java
+++ src/main/java/com/srivalli/travelagency/dto/CustomerDto.java
@@ -1,5 +1,8 @@
 package com.srivalli.travelagency.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,27 +15,41 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 public class CustomerDto {
+	@Min(value = 0L, message = "Id can not be negative")
 	private Long id;
 
+	@Pattern(regexp = "^[a-zA-Z ]$")
 	private String firstName;
 
+	@Pattern(regexp = "^[a-zA-Z ]$")
 	private String lastName;
 
-	private String permanentAddress;
+	@Min(value = 0, message = "Cost can not be negative")
+	private int cost;
 
-	private String communicationAddress;
-
+	@Pattern(regexp = "^[0-9]{10}$", message = "Phone number should be of 10 digits")
+	@NotBlank
 	private String phone;
 
+	@Pattern(regexp = "^[0-9a-zA-Z-/_\\s,]*[a-zA-Z]{3}[0-9a-zA-Z-/_\\s,]*$", message = "Invalid address format.")
+	@NotBlank
+	private String permanentAddress;
+
+	@Pattern(regexp = "^[0-9a-zA-Z-/_\\s,]*[a-zA-Z]{3}[0-9a-zA-Z-/_\\s,]*$", message = "Invalid address format.")
+	@NotBlank
+	private String communicationAddress;
+
+	@Pattern(regexp = "^[0-9a-zA-Z-/_\\s,]*[a-zA-Z]{3}[0-9a-zA-Z-/_\\s,]*$", message = "Invalid address format.")
+	@NotBlank
 	private String startingLocation;
 
+	@Pattern(regexp = "^[0-9a-zA-Z-/_\\s,]*[a-zA-Z]{3}[0-9a-zA-Z-/_\\s,]*$", message = "Invalid address format.")
+	@NotBlank
 	private String destinationLocation;
 
 	private String locations;
 
 	private String packageName;
-
-	private int cost;
 
 	private String notes;
 }
--- src/main/java/com/srivalli/travelagency/exceptionhandling/TravelControllerAdvice.java
+++ src/main/java/com/srivalli/travelagency/exceptionhandling/TravelControllerAdvice.java
@@ -1,34 +1,104 @@
 package com.srivalli.travelagency.exceptionhandling;
 
 import java.sql.SQLIntegrityConstraintViolationException;
+import java.time.format.DateTimeParseException;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.NoSuchElementException;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.srivalli.travelagency.errorhandling.ErrorMessage;
 
-@ControllerAdvice
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+
+@RestControllerAdvice
 public class TravelControllerAdvice {
 
 	@ExceptionHandler(SQLIntegrityConstraintViolationException.class)
 	@ResponseStatus(HttpStatus.CONFLICT)
 	public ResponseEntity<ErrorMessage> integrityConstraintViolationException(
 			SQLIntegrityConstraintViolationException ex) {
-		ErrorMessage errorMessage = new ErrorMessage(HttpStatus.CONFLICT, new Date(), "Duplicate Record",
-				"Phone Number Should be Unique");
+
+		ErrorMessage errorMessage = new ErrorMessage(HttpStatus.CONFLICT, new Date(), "Duplicate Record.",
+				"Data entered is already in use.");
+
 		return new ResponseEntity<ErrorMessage>(errorMessage, HttpStatus.CONFLICT);
 	}
 
 	@ExceptionHandler(NoSuchElementException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
-	public ResponseEntity<ErrorMessage> noSuchElementException(SQLIntegrityConstraintViolationException ex) {
+	public ResponseEntity<ErrorMessage> noSuchElementException(NoSuchElementException ex) {
+
 		ErrorMessage errorMessage = new ErrorMessage(HttpStatus.NOT_FOUND, new Date(), "Record not found",
-				"Record not found");
+				"Data does not exist for the given entry.");
+
 		return new ResponseEntity<ErrorMessage>(errorMessage, HttpStatus.NOT_FOUND);
+	}
+
+	@ExceptionHandler(DateTimeParseException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ResponseEntity<ErrorMessage> invalidDateException(DateTimeParseException ex) {
+
+		ErrorMessage errorMessage = new ErrorMessage(HttpStatus.BAD_REQUEST, new Date(), "Invalid Date",
+				"Please enter a valid date in the format dd/mm/yyyy");
+
+		return new ResponseEntity<ErrorMessage>(errorMessage, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ResponseEntity<ErrorMessage> validationBindException(MethodArgumentNotValidException exception,
+			HttpServletRequest request) {
+
+		String bindException = exception.getBindingResult().getFieldErrors().stream().reduce("Invalid ",
+				(x, fieldError) -> x + fieldError.getRejectedValue() + " value submitted for " + fieldError.getField()
+						+ ", ",
+				(a, b) -> a + b);
+
+		ErrorMessage errorMessage = new ErrorMessage(HttpStatus.BAD_REQUEST, new Date(), "Data could not be validated.",
+				bindException);
+
+		return new ResponseEntity<ErrorMessage>(errorMessage, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(ConstraintViolationException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ResponseEntity<Set<String>> constraintViolationException(ConstraintViolationException exception,
+			HttpServletRequest request) {
+
+		Set<ConstraintViolation<?>> constraintViolations = exception.getConstraintViolations();
+
+		Set<String> messages = new HashSet<>(constraintViolations.size());
+		messages.addAll(constraintViolations.stream()
+				.map(constraintViolation -> String.format("%s value '%s' %s", constraintViolation.getPropertyPath(),
+						constraintViolation.getInvalidValue(), constraintViolation.getMessage()))
+				.collect(Collectors.toList()));
+
+//		ErrorMessage errorMessage = new ErrorMessage(HttpStatus.BAD_REQUEST, new Date(),
+//				"Constraint could not be validated", messages);
+
+		return new ResponseEntity<>(messages, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ResponseEntity<ErrorMessage> httpMessageParseException(HttpMessageNotReadableException exception,
+			HttpServletRequest request) {
+
+		ErrorMessage errorMessage = new ErrorMessage(HttpStatus.BAD_REQUEST, new Date(), "Data could not be parsed.",
+				exception.getMessage());
+
+		return new ResponseEntity<ErrorMessage>(errorMessage, HttpStatus.BAD_REQUEST);
 	}
 }
